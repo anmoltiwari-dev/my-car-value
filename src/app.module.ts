3819208d7bfead6fa53797118eaa5f21d7a90fb0
anmoltiwari0225 <anmoltiwari0225@gmail.com>
2023-03-16 00:43:59 +0530

    typeorm migration configs

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,6 +9,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './users/user.entity';
 import { Report } from './reports/report.entity';
 const cookieSession = require('cookie-session');
+import * as ormconfig from '../ormconfig';
 
 @Module({
   imports: [
@@ -16,23 +17,7 @@ const cookieSession = require('cookie-session');
       isGlobal: true,
       envFilePath: `.env.${process.env.NODE_ENV}`,
     }),
-    TypeOrmModule.forRootAsync({
-      inject: [ConfigService],
-      useFactory: (config: ConfigService) => {
-        return {
-          type: 'sqlite',
-          database: config.get<string>('DB_NAME'),
-          synchronize: true,
-          entities: [User, Report],
-        };
-      }
-    }),
-    // TypeOrmModule.forRoot({
-    //   type: 'sqlite',
-    //   database: 'db.sqlite',
-    //   entities: [User, Report],
-    //   synchronize: true,
-    // }),
+    TypeOrmModule.forRoot(ormconfig),
     UsersModule,
     ReportsModule,
   ],
@@ -48,10 +33,12 @@ const cookieSession = require('cookie-session');
   ],
 })
 export class AppModule {
+
+  constructor(private configService: ConfigService) {}
   configure(consumer: MiddlewareConsumer) {
     consumer.apply(
       cookieSession({
-        keys: ['asdfasfd'],
+        keys: [this.configService.get('COOKIE_KEY')],
       })
     ).forRoutes('*');
   }
